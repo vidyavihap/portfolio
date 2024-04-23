3b72c58f193b73123a051f595cd1338af36f875e
vidyavihap <vidyap1004@gmail.com>
2024-04-23 13:48:31 -0500

    fallback route update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'home'},
-    { path: 'home', component: HomeComponent }
+    { path: '', pathMatch: 'full', redirectTo: '/home'},
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '/home' }
 ];
