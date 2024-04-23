cce8c545f4bc28e00eb8c0cb4c6f608099ea8ffe
vidyavihap <vidyap1004@gmail.com>
2024-04-23 12:15:33 -0500

    updated routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home'},
-    { path: 'home', component: HomeComponent },
-    { path: '**', pathMatch: 'full', redirectTo: ''}
+    { path: 'home', component: HomeComponent }
 ];
